61c2099bb86cd5e7d202a6a56328a8ada67ca930
Thendo Ramashia <84157694+MansaRon@users.noreply.github.com>
2022-04-22 23:46:38 +0200

    added lodash, jwt and bcryptjs packages, added authentication, registration and user login.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const router = require('./routes/routes');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 
 app.use(express.json());
 app.use(router);
@@ -19,23 +19,6 @@ app.use(cors(corsOptions));
 // Load middleware
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-    // res.header('Access-Control-Allow-Origin', '*');
-    // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    // res.header('Access-Control-Allow-Credentials', true);
-    // if (req.method === 'OPTIONS') {
-    //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    //     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-    //     return res.status(200).json({});
-    // };
-    console.log('Inside CORS...');
-    next();
-});
-
-// Test Route
-// app.get('/', (res, req) => {
-//     req.send('Hello world');
-// })
 
 // Database Connection
 const mongoString = 'mongodb+srv://MansaRon:TheGreat%4095@thendocluster.1gxlw.mongodb.net/TaskManager';
--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const cors = require('cors')
-const router = express.Router()
-module.exports = router;
 const TaskModel = require('../db/models/task.model');
 const ListModel = require('../db/models/list.model');
+const UserModel = require('../db/models/user.model');
 const app = express();
-
+const bodyParser = require('body-parser');
+const router = express.Router();
+module.exports = router;
 app.use(cors())
+// Load middleware
+app.use(bodyParser.json());
 
 // CORS handling
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id");
     res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Expose-Headers', 'x-access-token, x-refresh-token');
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id, Authorization');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, HEAD, OPTIONS, PUT, DELETE, PATCH');
         return res.status(200).json({});
     };
     next();
@@ -144,5 +148,80 @@ router.delete('/lists/:listId/tasks/:taskId', async (req, res) => {
     }
 })
 // Getting tasks under specific list
+
+// User Routes
+// Sign Up
+router.post('/users', (req, res) => {
+    let body = req.body;
+    let newUser = new UserModel(body);
+
+    newUser.save().then(() => { return newUser.createSession()
+    }).then((refreshToken) => { return newUser.generateAccessAuthToken(
+    ).then((accessToken) => { return {accessToken, refreshToken}})
+    }).then((authTokens) => { res.header('x-refresh-token', authTokens.refreshToken).header('x-access-token', authTokens.accessToken).send(newUser);
+    }).catch((err) => { res.status(400).send(err) })
+})
+// Sign Up
+
+// Login
+router.post('/users/login', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    UserModel.findByCredentials(email, password).then((user) => {
+        return user.createSession().then((refreshToken) => { return user.generateAccessAuthToken().then((accessToken) => { return { accessToken, refreshToken }});
+    }).then((authTokens) => { res.header('x-refresh-token', authTokens.refreshToken).header('x-access-token', authTokens.accessToken).send(user) })
+    }).catch((e) => { res.status(400).send(e) });
+})
+// Login
+
+// User Routes
+
+let verifySession = (req, res, next) => {
+    let refreshToken = req.header('x-refresh-token');
+    let _id = req.header('_id');
+
+    UserModel.findByIdAndToken(_id, refreshToken).then((user) => {
+        if (!user) {
+            return Promise.reject({'error': 'User not found. MAke sure refresh token and ID are correct.'});
+        }
+
+        let isSessionValid = false;
+
+        // If user is found, therefore session is valid
+        req.user_id = user._id;
+        req.refreshToken = refreshToken;
+        req.userObject = user;
+
+        user.sessions.forEach((session) => {
+            if (session.token == refreshToken) {
+                if (UserModel.hasRefreshTokenExpired(session.expires) == false) {
+                    // Refersh token
+                    isSessionValid = true;
+                }
+            }
+        });
+
+        if (isSessionValid) {
+            // VAlid session
+            next();
+        } else {
+            return Promise.reject({ 'error': 'Refersh token expired or invalid.' });
+        }
+
+    }).catch((err) => {
+        res.status(401).send(err);
+    })
+}
+
+// Gets Access Token
+router.get('/users/me/access-token', verifySession, (req, res) => {
+    req.userObject.generateAccessAuthToken().then((accessToken) => {
+        res.header('x-access-token', accessToken).send({ accessToken });
+    }).catch((err) => {
+        res.status(400).send(err);
+    })
+})
+
 
 
